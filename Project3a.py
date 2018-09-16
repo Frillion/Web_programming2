90a4502c7407d4b950d58ad3ea3fe65911b872ce
Frillion <fridriksoebech@gmail.com>
2018-09-16 20:59:12 +0000

    fixing problems with the actual program

--- Project3a.py
+++ Project3a.py
@@ -2,28 +2,28 @@ from bottle import *
 from sys import argv
 @route('/')
 def index():
-    info = {'home':'Home','info':{'Friðrik':'0908012440','Ari':'0605025169'},'Name':'Home'}
+    info = {'home':'Home','info':{'Friðrik':'0908012440','Ari':'0605025169'},'Name':'Home','Sum':'none'}
     return template('index.tpl',info)
 @route('/<page>')
 def KT(page):
     info = {'teacher':'Gunnar Þórunnarson','developer':'Friðrik Fannar Söebech','home': 'Home', 'info': {'Friðrik': '0908012440', 'Ari': '0605025169','Daníel':'2105012230'},'Name':'Home','Sum':'none'}
     if page == info['info']['Friðrik']:
-        sum = 0
+        summa = 0
         for i in page:
-                sum = sum+int(i)
-        info['Sum'] = str(sum)
+            summa = summa+int(i)
+        info['Sum'] = str(summa)
         info['Name'] = 'Friðrik'
     if page == info['info']['Ari']:
-        sum = 0
+        summa = 0
         for i in page:
-            sum = sum + int(i)
-        info['Sum'] = str(sum)
+            summa = summa + int(i)
+        info['Sum'] = str(summa)
         info['Name'] = 'Ari'
     if page == info['info']['Daníel']:
-        sum = 0
+        summa = 0
         for i in page:
-            sum = sum + int(i)
-        info['Sum'] = str(sum)
+            summa = summa + int(i)
+        info['Sum'] = str(summa)
         info['Name'] = 'Daníel'
     return template('index.tpl',info)
 run(host='0.0.0.0', port=argv[1], reloader=True, debug=True)
